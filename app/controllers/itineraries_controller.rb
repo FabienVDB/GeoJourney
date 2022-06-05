a28a2ab11700072c07089d5b7b1bae1bac402f63
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-06-05 13:52:16 +0200

    search bar fix complete

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -4,29 +4,16 @@ class ItinerariesController < ApplicationController
   def index
     @areas = Area.all
     @themes = Theme.all
+    @durations = Itinerary.all.map(&:duration_in_days).uniq.sort.map { |d| Duration.new(d) }
     if params[:search].present?
-      areas = selections('area')
-      themes = selections('theme')
-
-      @itineraries_by_areas = itineraries_by_multiple(areas)
-      @itineraries_by_themes = itineraries_by_multiple(themes)
-
-      @itineraries = @itineraries_by_areas.intersection(@itineraries_by_themes)
-
+      @itineraries = filter_itineraries
     else
       @itineraries = Itinerary.all
     end
 
-    @itineraries_for_mapbox = @itineraries.map do |itinerary|
-      {
-        name: itinerary.name,
-        summary: itinerary.summary,
-        sites: itinerary.sites.sort_by(&:stage).map { |s| { stage: s.stage, lat: s.latitude, lng: s.longitude } },
-        # info_window: render_to_string(partial: "shared/card_itinerary_index", locals: { itinerary: itinerary }),
-        info_window: render_to_string(partial: "shared/info_window_itinerary", locals: { itinerary: itinerary }),
-        image_url: helpers.cl_image_path(itinerary.photo.key)
-      }
-    end
+    @itineraries_for_mapbox = itineraries_to_json(@itineraries)
+
+    # @markers = @itineraries_for_mapbox.map { |i| i[:sites] }.flatten
   end
 
   def show
@@ -35,15 +22,58 @@ class ItinerariesController < ApplicationController
 
   private
 
-  def selections(params_key)
-    values = params[:search][params_key.to_s].reject(&:empty?)
-    values = params_key.capitalize.constantize.all.map(&:name) if values.empty?
+  def filter_itineraries
+    areas = selections(:areas, :name)
+    themes = selections(:themes, :name)
+    durations = selections(:durations, :days)
+
+    itineraries_by_areas = filter_by(areas)
+    itineraries_by_themes = filter_by(themes)
+    itineraries_by_durations = filter_by(durations)
+
+    itineraries_by_areas.intersection(itineraries_by_themes, itineraries_by_durations)
+  end
+
+  def selections(params_key, field)
+    values = params[:search][params_key].reject(&:empty?)
+    if params_key == :durations && values.empty?
+      values = @durations.map { |s| s.public_send(field) }
+    elsif params_key == :durations
+      values = values.map(&:to_i)
+    elsif values.empty?
+      values = params_key.to_s.chop.capitalize.constantize.all.map { |s| s[field] }
+    end
     values
   end
 
-  def itineraries_by_multiple(selections)
-    @itineraries_by_selections = []
-    selections.each { |selection| @itineraries_by_selections += Itinerary.global_search(selection) }
-    @itineraries_by_selections
+  def filter_by(selections)
+    filtered_itineraries = []
+    selections.each { |selection| filtered_itineraries += Itinerary.global_search(selection) }
+    filtered_itineraries
+  end
+
+  def itineraries_to_json(itineraries)
+    itineraries.map do |itinerary|
+      {
+        name: itinerary.name,
+        summary: itinerary.summary,
+        sites: itinerary.sites.sort_by(&:stage).map { |s| { stage: s.stage, lat: s.latitude, lng: s.longitude } },
+        # info_window: render_to_string(partial: "shared/card_itinerary_index", locals: { itinerary: itinerary }),
+        info_window: render_to_string(partial: "shared/info_window_itinerary", locals: { itinerary: itinerary }),
+        image_url: helpers.cl_image_path(itinerary.photo.key)
+      }
+    end.to_json
+  end
+
+  class Duration
+    attr_accessor :days
+
+    def initialize(days)
+      @days = days
+    end
+
+    def to_s
+      "#{days} day#{days > 1 ? 's' : ''}"
+    end
   end
 end
