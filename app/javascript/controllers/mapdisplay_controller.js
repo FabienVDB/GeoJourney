b5825735d4b84f15d75b19acb1b23c1ca4feb1c6
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-06-06 19:12:14 +0200

    Itineraries map position fixed

--- app/javascript/controllers/mapdisplay_controller.js
+++ app/javascript/controllers/mapdisplay_controller.js
@@ -1,12 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "displayed" ]
+  static targets = [ "btn", "mapcontainer", "cardscontainer" ]
   // connect() {
   //   console.log("Hello from our first Stimulus controller")
   // }
   display(event){
       event.preventDefault()
-      this.displayedTarget.classList.toggle("d-none")
+      this.btnTarget.innerHTML = this.btnTarget.innerHTML === "Map" ? "List" : "Map";
+      this.#toggleScroll()
+      this.mapcontainerTarget.classList.toggle("d-none")
+      this.cardscontainerTarget.classList.toggle("d-none")
     }
+
+  #toggleScroll() {
+      document.body.classList.toggle("stop-scrolling");
+  }
+
 }
