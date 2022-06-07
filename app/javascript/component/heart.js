645aaab9b44a2720e6b9079c686ffc5fccb86ee6
zanoune kalid <zanounekalid@hotmail.fr>
2022-06-07 09:25:46 +0200

    repmenu

--- app/javascript/component/heart.js
+++ app/javascript/component/heart.js
@@ -1,12 +1,12 @@
-let favoriteElement = document.querySelector(".m-favorite");
+// let favoriteElement = document.querySelector(".m-favorite");
 
-favoriteElement.addEventListener("click",function(e){
-   console.log(favoriteElement.className);
-  if (favoriteElement.className.indexOf("-active") > -1) {
-    favoriteElement.classList.remove("-active");
-  } else {
-    favoriteElement.classList.add("-active");
-  }
+// favoriteElement.addEventListener("click",function(e){
+//    console.log(favoriteElement.className);
+//   if (favoriteElement.className.indexOf("-active") > -1) {
+//     favoriteElement.classList.remove("-active");
+//   } else {
+//     favoriteElement.classList.add("-active");
+//   }
 
-},false);
-export { heart };
+// },false);
+// export { heart };
